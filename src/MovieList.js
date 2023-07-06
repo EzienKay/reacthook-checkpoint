212b8e3f65fa107c92a77896aa73856f0db31760
EzienKay <kleeqers@gmail.com>
2023-07-06 13:58:13 +0100

    first commit

---
+++ src/MovieList.js
@@ -0,0 +1,41 @@
+
+
+const MovieList = [
+    {
+        id: 1,
+        img: 'https://static.netnaija.com/i/Wz27WpgBa5R.webp',
+        title: 'Hijack', 
+        description: "Let them think they're in control. When Flight KA29 is hijacked during its seven-hour journey from Dubai to London, Sam Nelson-an accomplished corporate negotiator-tries using his professional skills to save everyone on board. Will this high-risk strategy be his undoing?", 
+        posterURL: 'https://www.thenetnaija.net/videos/series/18968-hijack', 
+        rating: '2'    
+    },
+    {
+        id: 2,
+        img: 'https://static.netnaija.com/i/qvg70e50aze.webp',
+        title: 'HeartBeat', 
+        description: 'Ah.. I told you not to open it until 100 years. Half-human and half-vampire Seon Woo-hyeol, who was unable to become human due to a one day difference amidst 100 years, starts to live together with Joo In-hae, a woman who has no humane side, and find true warmth.', 
+        posterURL: 'https://www.thenetnaija.net/videos/kdrama/18958-heartbeat', 
+        rating: '1'
+    
+    },
+    {
+        id: 3,
+        img: 'https://static.netnaija.com/i/BQJNjJrDNrL.png',
+        title: '5 Days In The Prison', 
+        description: 'Popular Nigerian comedian and skit-maker, Brain Jotter is out with a new episode of his comedy series titled "5 Days In The Prison".', 
+        posterURL: 'https://www.thenetnaija.net/videos/nollywood/18862-5-days-in-the-prison-2023', 
+        rating: '2'
+    
+    },
+    {
+        id: 4,
+        img: 'https://static.netnaija.com/i/no4NX2LL79M.webp',
+        title: 'Good Night', 
+        description: 'Mohan, an IT youngster with a snoring issue starts his life with Anu, the girl of his dreams, hiding his issue. However, trouble begins, when his snoring starts affecting his life and the health of his partner', 
+        posterURL: 'https://www.thenetnaija.net/videos/movies/18993-good-night-2023-indian', 
+        rating: '1'
+    
+    }
+]
+
+export default MovieList
